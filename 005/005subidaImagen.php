7a533a28c2693d8a863068d99322a2163500cf7b
ViictorTorres21 <victor.torres.ortiz.18@gmail.com>
2022-11-18 13:56:20 +0100

    004 Editado y 005 HECHO

--- 005/005subidaImagen.php
+++ 005/005subidaImagen.php
@@ -8,11 +8,15 @@ $nombre_archivo = $_FILES['userfile']['name'];
 $tipo_archivo = $_FILES['userfile']['type'];
 $tamano_archivo = $_FILES['userfile']['size'];
 	
-//Comprobación subida de archivo
+//Comprobación extensión
+if (!((strpos($tipo_archivo, "gif") || strpos($tipo_archivo, "jpeg")) && ($tamano_archivo < 100000))) {
+   	echo "La extensión o el tamaño de los archivos no es correcta. <br><br><table><tr><td><li>Se permiten archivos .gif o .jpg<br><li>se permiten archivos de 100 Kb máximo.</td></tr></table>";
+}else{
+    //Comprobación subida archivos
    	if (move_uploaded_file($_FILES['userfile']['tmp_name'],  $nombre_archivo)){
       		echo "Archivo Cargado";
    	}else{
       		echo "ERROR al subir el archivo";
    	}
-
+}
 
